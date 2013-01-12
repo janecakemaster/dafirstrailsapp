a1b21a1fc1d6f27edf63be5fd4374e0f5482987a
Jane Cakemaster <janecakemaster@gmail.com>
2013-01-12 00:10:08 -0500

    the guts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dafirstrailsapp::Application.config.secret_token = 'b0f4903cf752a609fd43efd61df16876c490744fecf29fa4241704c6ba60fb899e1514653259d1d37e5a9b1f7f6acf79010ea05ea99d21dc0e9be91b3ad39f27'
